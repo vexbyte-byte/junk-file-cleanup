37dbe40814a9412f1b92be8ddfa80a371f9c1026
Windows 11 22H2 <laserminigun1@gmail.com>
2025-09-21 13:10:23 +0800

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -76,6 +76,23 @@ directory_list = [ # you can add more
     r"/storage/emulated/0/Pictures/.thumbnails",
     r"/storage/emulated/0/Pictures/stickers",
     r"/storage/emulated/0/Pictures/stickers_renamed",
+
+    # nomedia files
+    r'/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Audio/Sent/.nomedia', 
+    r'/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Audio/Private/.nomedia', 
+    r'/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp AI Media/.nomedia', 
+    r'/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Animated Gifs/Sent/.nomedia', 
+    r'/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Animated Gifs/Private/.nomedia', 
+    r'/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Voice Notes/.nomedia', 
+    r'/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/.Links/.nomedia', 
+    r'/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/.Statuses/.nomedia', 
+    r'/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/.udDHFY8K4Eqg/.nomedia', 
+    r'/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Backup Excluded Stickers/.nomedia', 
+    r'/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Bug Report Attachments/.nomedia', 
+    r'/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/.Shared/.nomedia', 
+    r'/storage/emulated/0/Android/.Trash/com.sec.android.app.myfiles/.nomedia', 
+    r'/storage/emulated/0/Android/.Trash/com.sec.android.app.myfiles/11b3e3d6-43e3-4a6b-b4dd-441f6036b93bT3/1758423551688/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Backup Excluded Stickers/.!%#@$/.nomedia',
+
 ]
 
 
@@ -134,19 +151,76 @@ def main():
 
 
 def find_nomedia(root_path):
+    print(f"{b_green}[*]{d_green} Searching for .nomedia under: {SEARCH_ROOT}\n")
     """Recursively walk through root_path and print all .nomedia files."""
     for dirpath, dirnames, filenames in os.walk(root_path):
         for name in filenames:
             if name == ".nomedia":
                 nomedia_path = os.path.join(dirpath, name)
                 if nomedia_path not in directory_list:
-                    print(f"{b_green}[+] {nomedia_path}")
+                    print(f"{b_green}[+]{d_green} {nomedia_path}")
+    print(f"\n{b_green}[+]{d_green} Search complete.")
+    
 
+def sanitize():
+    text = "[+] /storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Audio/Sent/.nomedia [+] /storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Audio/Private/.nomedia [+] /storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp AI Media/.nomedia [+] /storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Animated Gifs/Sent/.nomedia [+] /storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Animated Gifs/Private/.nomedia [+] /storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Voice Notes/.nomedia [+] /storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/.Links/.nomedia [+] /storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/.Statuses/.nomedia [+] /storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/.udDHFY8K4Eqg/.nomedia [+] /storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Backup Excluded Stickers/.nomedia [+] /storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Bug Report Attachments/.nomedia [+] /storage/emulated/0/Android/media/com.whatsapp/WhatsApp/.Shared/.nomedia [+] /storage/emulated/0/Android/.Trash/com.sec.android.app.myfiles/.nomedia [+] /storage/emulated/0/Android/.Trash/com.sec.android.app.myfiles/11b3e3d6-43e3-4a6b-b4dd-441f6036b93bT3/1758423551688/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Backup Excluded Stickers/.!%#@$/.nomedia"
+    paths = [p.strip() for p in text.split("[+]") if p.strip()]
+    lists = []
+    for i in paths:
+        lists.append(i)
+    print(lists)
+
+def logo():
+    text = r"""
+     ________  ___       _______   ________  ________   ___  ___  ________   
+    |\   ____\|\  \     |\  ___ \ |\   __  \|\   ___  \|\  \|\  \|\   __  \  
+    \ \  \___|\ \  \    \ \   __/|\ \  \|\  \ \  \\ \  \ \  \\\  \ \  \|\  \ 
+     \ \  \    \ \  \    \ \  \_|/_\ \   __  \ \  \\ \  \ \  \\\  \ \   ____\
+      \ \  \____\ \  \____\ \  \_|\ \ \  \ \  \ \  \\ \  \ \  \\\  \ \  \___|
+       \ \_______\ \_______\ \_______\ \__\ \__\ \__\\ \__\ \_______\ \__\   
+        \|_______|\|_______|\|_______|\|__|\|__|\|__| \|__|\|_______|\|__|                                                          
+    """
+    print(text)
+    print()
+
+def selection():
+    while True:
+        # list menu
+        # input
+        logo()
+        print(f"{b_green}[{d_yellow}01{b_green}]{d_green} Search for .nomedia files that aren't included in database")
+        print(f"{b_green}[{d_yellow}02{b_green}]{d_green} Search for broken directories in database")
+        print(f"{b_green}[{d_yellow}03{b_green}]{d_green} Remove junk files")
+        print(red)
+        print()
+        choice = str(input(f"[Main Menu] > {d_yellow}"))
+        clear_screen()
+        logo()
+        # filter
+        if choice.startswith("0") and len(choice) > 1:
+            choice = choice[-1]
+        
+        options = {
+            "1": "find_nomedia(SEARCH_ROOT)",
+            "2": "detect_directories()",
+            "3": "main()"
+        }
+
+        value = options.get(choice)
+        exec(value)
+        input()
+        clear_screen()
+
+
+def clear_screen():
+    try:
+        os.system("cls" if os.name == "nt" else "clear")
+        print("\033c\033[0m", end="")
+    except Exception as e:
+    	pass
 
 
 if __name__ == "__main__":
-    # main()
-    # detect_directories()
-    print(f"Searching for .nomedia under: {SEARCH_ROOT}\n")
-    find_nomedia(SEARCH_ROOT)
-    print("\nSearch complete.")
+    clear_screen()
+    selection()
+
