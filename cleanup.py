553ec0d800cb94f239f83d584b05a4fef7d95356
Windows 11 22H2 <laserminigun1@gmail.com>
2025-09-25 17:09:59 +0800

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -6,9 +6,6 @@ import shutil
 from datetime import datetime
 # from send2trash import send2trash as delete
 
-# base:
-SEARCH_ROOT = r"/storage/emulated/0"
-
 # define variables
 red = "\033[91m"
 b_green = "\033[92m"
@@ -16,7 +13,14 @@ d_green = "\033[32m"
 b_yellow = "\033[93m"
 d_yellow = "\033[33m"
 
+file_deleted_count = 0
+folder_deleted_count = 0
+failed_items = 0
+
 # list of directories to clean
+temp_directory_list = []
+files_list = []
+folders_list = []
 directory_list = [ # you can add more
     # whatsapp junk files
     r"/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/.Shared",
@@ -92,9 +96,9 @@ directory_list = [ # you can add more
     r'/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Backup Excluded Stickers/.nomedia', 
     r'/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Bug Report Attachments/.nomedia', 
     r'/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/.Shared/.nomedia', 
-    r'/storage/emulated/0/Android/.Trash/com.sec.android.app.myfiles/.nomedia', 
     r'/storage/emulated/0/Android/.Trash/com.sec.android.app.myfiles/11b3e3d6-43e3-4a6b-b4dd-441f6036b93bT3/1758423551688/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Backup Excluded Stickers/.!%#@$/.nomedia',
-
+    r'/storage/emulated/0/Android/.Trash/com.sec.android.app.myfiles/.nomedia', 
+    # r'C:\Users\Wei Jie\Documents', # debug
 ]
 
 class utils():
@@ -112,6 +116,8 @@ class utils():
         print()
 
     def selection():
+        engine.explore_subfolders()
+        engine.filter_directories()
         while True:
             # list menu
             # input
@@ -129,14 +135,14 @@ class utils():
                 choice = choice[-1]
             
             options = {
-                "1": "engine.find_nomedia(SEARCH_ROOT)",
-                "2": "engine.detect_directories()",
-                "3": "engine.main()"
+                "1": engine.find_nomedia,
+                "2": engine.detect_directories,
+                "3": engine.main
             }
 
             try:
                 value = options.get(choice)
-                exec(value)
+                value()
             except:
                 utils.clear_screen()
                 continue
@@ -173,52 +179,96 @@ class engine():
         print(a, "\n", "-" * len(a))
         for good_path in good_dir:
             print(f"{d_green}[+]{b_green} ", good_path)
-
-
-    def main():
-        # variables
-        initial_time = time.time()
-        var_1, var_2, var_3 = 30,10,20
-        deleted_items = []
-        total_items = int(len(directory_list))
-        file_deleted_count = 0
-        folder_deleted_count = 0
-
+    
+    def filter_directories():
+        global failed_items
         for directory in directory_list:
-            now = datetime.now()
-            formatted_time = now.strftime("%H:%M:%S")
-            if not os.path.exists(directory):
-                print(f"{red}[{formatted_time}] Directory does not exist: {directory[-20:]}")
-                continue # skip if file not found
-
-            # for file_name in os.listdir(directory):
-            # file_path = os.path.join(directory, file_name)
             try:
+                if not os.path.exists(directory):
+                    failed_items += 1
+                    continue
                 # Delete files only
                 if os.path.isfile(directory):
-                    try:
-                        # os.remove(directory)
-                        print(f"{b_green}[{d_yellow}{formatted_time}{b_green}] Successfully Removed: {directory}")
-                        file_deleted_count += 1
-                    except Exception as e:
-                        print(f"{red}[{formatted_time}] ", e)
+                    try: files_list.append(directory)
+                    except: pass
 
                 # Delete empty directories
                 elif os.path.isdir(directory):
-                    try:
-                        # shutil.rmtree(directory)
-                        print(f"{b_green}[{d_yellow}{formatted_time}{b_green}] Successfully Removed Empty Dir: {directory}")
-                        folder_deleted_count += 1
-                    except Exception as e:
-                        print(f"{red}[{formatted_time}] ", e)
-                try:
-                    percentage = (int(len(deleted_items))/total_items) * 100 # total_items is already integer
-                    elapsed_time = time.time() - initial_time
-                except:
-                    pass
+                    try: folders_list.append(directory)
+                    except: pass
+            except:
+                pass
+        
+        # sorting directories by depth
+        try: folders_list.sort(key=lambda x: x.count(os.sep), reverse=True)
+        except: pass
 
+    def explore_subfolders():
+        for directory in directory_list:
+            for root, dirs, files in os.walk(directory, topdown=False):
+                for file_name in files:
+                    file_path = os.path.join(root, file_name)
+                    try: temp_directory_list.append(file_path)
+                    except: pass
+                
+                for dir_name in dirs:
+                    dir_path = os.path.join(root, dir_name)
+                    try: temp_directory_list.append(dir_path)
+                    except: pass
+        # combine
+        directory_list.extend(temp_directory_list)
+        temp_directory_list.clear()
+
+    def main():
+        # variables
+        global failed_items
+        initial_time = time.time()
+        var_1, var_2, var_3 = 30,10,20
+        print(len(directory_list))
+        os.system('pause')
+        total_items = int(len(directory_list))
+
+        # delete files
+        for directory in files_list:
+            now = datetime.now()
+            formatted_time = now.strftime("%H:%M:%S")
+            # i know i already had this check earlier
+            # but it doesnt mean a path would continue to be present during runtime
+            if not os.path.exists(directory):
+                print(f"{red}[{formatted_time}] Directory does not exist: {directory[-20:]}")
+                continue # skip if file not found
+            
+            try:
+                # os.remove(directory)
+                print(f"{b_green}[{d_yellow}{formatted_time}{b_green}] Successfully Removed: {directory}")
+                file_deleted_count += 1
+            except Exception as e:
+                print(f"{red}[{formatted_time}] ", e)
+            
+            # calculate percentage
+            try:
+                percentage = (file_deleted_count + folder_deleted_count - failed_items) / total_items * 100
+                elapsed_time = time.time() - initial_time
+            except:
+                pass
+
+        # Delete folders
+        for directory in folders_list:
+            try:
+                # shutil.rmtree(directory)
+                print(f"{b_green}[{d_yellow}{formatted_time}{b_green}] Successfully Removed Empty Dir: {directory}")
+                folder_deleted_count += 1
+            except Exception as e:
+                print(f"{red}[{formatted_time}] ", e)
+            
+            # calculate percentage
+            try:
+                percentage = (file_deleted_count + folder_deleted_count - failed_items) / total_items * 100
+                elapsed_time = time.time() - initial_time
+
+                # print
                 print(f"{b_green}\n")
-                print("".ljust(var_3), f"{b_green}Cleaning Up".ljust(var_1), f"{d_green}:".ljust(var_2), int(round(percentage)), "%")
+                print("".ljust(var_3), f"{b_green}Cleaning Up".ljust(var_1), f"{d_green}:".ljust(var_2), f"{int(round(percentage))}%")
                 print("".ljust(var_3), f"{b_green}Files deleted".ljust(var_1), f"{d_green}:".ljust(var_2), int(file_deleted_count))
                 print("".ljust(var_3), f"{b_green}Folders Deleted".ljust(var_1), f"{d_green}:".ljust(var_2), int(folder_deleted_count))
                 print("".ljust(var_3), f"{b_green}Time Taken (Seconds)".ljust(var_1), f"{d_green}:".ljust(var_2), int(round(elapsed_time)))
@@ -229,8 +279,9 @@ class engine():
         print(f"\n{d_green}[+]{b_green} Cleanup Complete!\n")
 
 
-    def find_nomedia(root_path):
-        print(f"{b_green}[*]{d_green} Searching for .nomedia under: {SEARCH_ROOT}\n")
+    def find_nomedia():
+        root_path = r"/storage/emulated/0"
+        print(f"{b_green}[*]{d_green} Searching for .nomedia under: {root_path}\n")
         """Recursively walk through root_path and print all .nomedia files."""
         for dirpath, dirnames, filenames in os.walk(root_path):
             for name in filenames:
